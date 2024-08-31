e552c9b4397665181fe509e6a42523afd1ded037
ungnome <73322863+ungnome@users.noreply.github.com>
2024-08-30 20:49:04 -0400

    feat: ui refresh

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,18 +2,16 @@
 	import { search, total } from './tabs';
 </script>
 
-<header
-	class="sticky top-0 z-50 w-screen flex flex-col content-center drp-shadow-sm bg-white dark:bg-neutral-800 dark:text-white">
-	<h2 class="m-3 text-2xl font-semibold">Tabitat</h2>
-
-	<input
-		type="search"
-		placeholder="Search"
-		bind:value={$search}
-		autofocus
-		class="mx-4 mb-4 p-1 border border-gray-300 rounded-lg text-base focus:outline-none focus:border-2 focus:border-indigo-400 focus:ring-indigo-200 dark:bg-neutral-800" />
-
-	<p class="self-center mb-1 text-base">Total Tabs: {$total}</p>
+<header>
+	<nav class="navbar">
+		<h2 class="m-3 text-2xl font-semibold">Tabitat</h2>
+		<input
+			class="input input-bordered input-primary input-sm w-full max-w-sm"
+			type="search"
+			placeholder="Search {$total} Tabs"
+			bind:value={$search}
+			autofocus />
+	</nav>
 </header>
 
 <style></style>
